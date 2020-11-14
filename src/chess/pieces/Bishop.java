3746caf67057680484da5312b813d0892eec510b
Vinicius Augusto <vinicius_augusto_@msn.com>
2020-11-14 20:11:42 -0300

    Bishop logic

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -17,7 +18,40 @@ public class Bishop extends ChessPiece{
 
 	@Override
 	public boolean[][] possibleMoves() {
-		// TODO Auto-generated method stub
-		return null;
+		boolean[][] matrix = new boolean[getBoard().getRows()][getBoard().getRows()];
+		Position p = new Position(0,0);
+		//right up
+		p.setValues(position.getRow() +1, position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() + 1);
+		} if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+		}
+		//left up
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() -1);
+		} if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+		}
+		//right down - nw
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() + 1);
+		} if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+		}
+		//down left
+		p.setValues(position.getRow() -1, position.getColumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() -1);
+		} if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+		}		
+		return matrix;
 	}
 }
